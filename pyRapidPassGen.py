5793d75463012f7d904e9a1feb19e194dede812a
Ryan Grieve <66217567+ryangrieve@users.noreply.github.com>
2022-06-17 10:24:11 -0600

    Refactoring

--- pyRapidPassGen.py
+++ pyRapidPassGen.py
@@ -2,41 +2,41 @@ from string import ascii_letters, digits, punctuation
 from secrets import choice
 
 print("\npyRapidPassGen - A simple password generator using python.\n")
-user_option = input("Begin options questionnaire? (y/n): ")
 
-if user_option == "y":
 
-    def user_config():
-        def error_message():  # Error message
-            print("\n**Invalid option response, please try again.**\n")
-            user_config()
+def user_config():
+    def error_message():  # Error message
+        print("\n**Invalid option response, please try again.**\n")
+        user_config()
 
-        print("\nPlease answer a few quick questions: \n")  # Questionnaire
+    print("\nPlease answer a few quick questions: \n")  # Questionnaire
+    try:
+        chars, number_pass, special_chars = (
+            int(input(">> How long should the password be? (eg: 20): ")),
+            int(input(">> How many passwords are required? (eg: 5): ")),
+            str(input(">> Use special characters? (y/n): ")),
+        )
+    except Exception:
+        error_message()
+
+    print("\nGenerating passwords...\n")  # Generator
+    for i in range(number_pass):
+        CHARACTERS = (ascii_letters, digits, punctuation)
         try:
-            chars, number_pass, special_chars = (
-                int(input(">> How long should the password be? (eg: 20): ")),
-                int(input(">> How many passwords are required? (eg: 5): ")),
-                str(input(">> Use special characters? (y/n): ")),
-            )
+            if special_chars == "y":
+                x = CHARACTERS
+            elif special_chars == "n":
+                x = CHARACTERS[0:1]
+            password = "".join(choice("".join(x)) for i in range(chars))
         except Exception:
             error_message()
+        print(password)
 
-        print("\nGenerating passwords...\n")  # Generator
-        for i in range(number_pass):
-            CHARACTERS = (ascii_letters, digits, punctuation)
-            try:
-                if special_chars == "y":
-                    x = [CHARACTERS[i] for i in [0, 1, 2]]
-                elif special_chars == "n":
-                    x = [CHARACTERS[i] for i in [0, 1]]
-                password = "".join(choice("".join(x)) for i in range(chars))
-            except Exception:
-                error_message()
-            print(password)
+    user_config() if input(  # Optional restart
+        "\npyRapidPassGen task complete.\n\nRestart? (y/n): "
+    ) == "y" else quit("pyRapidPassGen closing...")
 
-        restart = input("\npyRapidPassGen task complete.\n\nRestart? (y/n): ")
-        user_config() if restart == "y" else quit()
 
-    user_config()
-else:
-    quit("pyRapidPassGen closing...")
+user_config() if input("Begin user configuration? (y/n): ") == "y" else quit(
+    "pyRapidPassGen closing..."
+)
